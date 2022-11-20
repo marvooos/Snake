0cded1ad47008934dd75333297d6d6a3e3498672
Timurlan <magainvolter@gmail.com>
2022-11-20 16:07:33 +0300

    first commit

--- JS.js
+++ JS.js
@@ -1,28 +1,78 @@
+ const field = document.querySelector('.field');
+ const cellSize = 100;
 
-    const calculator = document.querySelector('.calculator');
-    const result = document.querySelector('#result');
+ const empty = {
+    value: 0,
+    top: 0,
+    left: 0
+ };
 
-    calculator.addEventListener('click', function(event) {
-        if(!event.target.classList.contains('calculator-btn')) return;
+ const cells = [];
+ cells.push(empty);
 
-        const value = event.target.innerText;
+ function move(index) { 
 
-            switch(value) {
-                case 'C':
-                    result.innerText = '';
-                    break;
+    const cell = cells[index]; 
 
-                    case '=':
-                        //защита eval
-            if(result.innerText.search(/[^0-9*/+-.]/mi) != -1) return;
+    const leftDiff = Math.abs(empty.left - cell.left);
+    const topDiff = Math.abs(empty.top - cell.top);
 
-                result.innerText = eval(result.innerText).toFixed(0);
-                        break;
+    if (leftDiff + topDiff > 1) { 
+       return;
+    }
 
-                        default:
-                            result.innerText += value;
-                }
+    cell.element.style.left = `${empty.left * cellSize}px`;
+    cell.element.style.top = `${empty.top * cellSize}px`;
 
-                
-       
+
+    const emptyLeft = empty.left;
+    const emptyTop = empty.top;
+    empty.left = cell.left;
+    empty.top = cell.top;
+    cell.left = emptyLeft;
+    cell.top = emptyTop;
+
+
+    const isFin = cells.every(cell => {
+       console.log(cell.value, cell.top, cell.left);
+       return cell.value === cell.top * 4 + cell.left;
     });
+
+
+    if (isFin) {
+       alert('YOU WIN');
+    }
+
+ }
+
+ const numbers =  [...Array(15).keys()]
+ .sort(() => Math.random() - 0.5);
+
+
+ for (let i = 1; i <= 15; i++) {
+
+    const cell = document.createElement('div');
+    const value = numbers[i - 1] + 1;
+    cell.className = 'cell';
+    cell.innerHTML = value;
+    const left = i % 4;
+    const top = (i - left) / 4;
+
+
+    cells.push({
+       value:value,
+       left: left,
+       top: top,
+       element: cell
+    });
+
+
+    cell.style.left = `${left * cellSize}px`;
+    cell.style.top = `${top * cellSize}px`;
+    
+    field.append(cell);
+
+    cell.addEventListener('click', () => {
+       move(i);
+    });
+ }
