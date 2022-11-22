c80aab426553e8f9386182da60b5a5dc65202ffb
Timurlan <magainvolter@gmail.com>
2022-11-22 21:54:07 +0300

    hh

--- JS.js
+++ JS.js
@@ -1,78 +1,47 @@
- const field = document.querySelector('.field');
- const cellSize = 100;
-
- const empty = {
-    value: 0,
-    top: 0,
-    left: 0
- };
-
- const cells = [];
- cells.push(empty);
-
- function move(index) { 
-
-    const cell = cells[index]; 
-
-    const leftDiff = Math.abs(empty.left - cell.left);
-    const topDiff = Math.abs(empty.top - cell.top);
-
-    if (leftDiff + topDiff > 1) { 
-       return;
-    }
-
-    cell.element.style.left = `${empty.left * cellSize}px`;
-    cell.element.style.top = `${empty.top * cellSize}px`;
+const slider = document.querySelector('.slider');
+const cell = document.querySelector('.cell');
+// const butNext = document.querySelector('.but-next');
+// const butPrev = document.querySelector('.but-prev');
 
 
-    const emptyLeft = empty.left;
-    const emptyTop = empty.top;
-    empty.left = cell.left;
-    empty.top = cell.top;
-    cell.left = emptyLeft;
-    cell.top = emptyTop;
+// const cellRed = document.querySelector('.cellRed');
+// const cellGreen = document.querySelector('.cellGreen');
+
+// //RED
+// function bodyColorRed() {
+// document.body.style.background = 'red';
+// };
+// cellRed.addEventListener('click',  () => {
+//    setTimeout (() => document.body.style.background = '', 2000);
+//    bodyColorRed();
+// });
 
 
-    const isFin = cells.every(cell => {
-       console.log(cell.value, cell.top, cell.left);
-       return cell.value === cell.top * 4 + cell.left;
-    });
+//GREEN
+// function bodyColorGreen() {
+// document.body.style.background = 'Green';
+// };
+// cellGreen.addEventListener('click',  () => {
+//    setTimeout (() => document.body.style.background = '', 3000);
+//    bodyColorGreen();
+// });
+let offset = 0;
+
+document.querySelector('.but-next').addEventListener('click', () => {
+   offset = offset + 400;
+   if (offset > 800) {
+     offset = 0;
+   };
+   slider.style.left = -offset + 'px';
+   
+});
+document.querySelector('.but-prev').addEventListener('click', () => {
+   offset = offset - 400;
+   if (offset < 0) {
+     offset = 800;
+   };
+   slider.style.left = -offset + 'px';
+   
+}); 
 
 
-    if (isFin) {
-       alert('YOU WIN');
-    }
-
- }
-
- const numbers =  [...Array(15).keys()]
- .sort(() => Math.random() - 0.5);
-
-
- for (let i = 1; i <= 15; i++) {
-
-    const cell = document.createElement('div');
-    const value = numbers[i - 1] + 1;
-    cell.className = 'cell';
-    cell.innerHTML = value;
-    const left = i % 4;
-    const top = (i - left) / 4;
-
-
-    cells.push({
-       value:value,
-       left: left,
-       top: top,
-       element: cell
-    });
-
-
-    cell.style.left = `${left * cellSize}px`;
-    cell.style.top = `${top * cellSize}px`;
-    
-    field.append(cell);
-
-    cell.addEventListener('click', () => {
-       move(i);
-    });
- }
